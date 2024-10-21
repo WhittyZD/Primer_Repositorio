a071eec38ab6a4e3a78055da1f524cc36f8171b3
ZoeDylan <zoe.velazquez.her@gmail.com>
2024-10-21 09:10:08 -0600

    Add files via upload

---
+++ P01Variables.php
@@ -0,0 +1,19 @@
+<?php
+$dia = 24; 
+$sueldo = 614122349.43;
+$nombre = "Mbappé";
+$exite = true;
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre;echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. " ".$cadena2;
+?>
