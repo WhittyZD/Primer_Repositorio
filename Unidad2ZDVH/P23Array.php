da742c1f7e341621824bf188250e2955fc39733c
ZoeDylan <zoe.velazquez.her@gmail.com>
2024-10-21 10:35:16 -0600

    Update P23Array.php

--- Unidad2ZDVH/P23Array.php
+++ Unidad2ZDVH/P23Array.php
@@ -1,4 +1,9 @@
 <?php
+/* CBTIS 89
+Programa que alamcena furutas para fruteria , tienda ,precios en un arreglo
+Velazquez Hernandez Zoe Dylan
+3°A Programación Matutino
+*/
 $fruteria = array("40$"=>'Manzana','10$'=>'Mangos','90$'=>'Limon','15$'=>'Naranjas');
 
 echo "** Fruteria Del Sur **","<br>","<br>";
