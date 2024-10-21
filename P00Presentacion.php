cec2cc821814c2cac0b2214553b09cffadb03127
ZoeDylan <zoe.velazquez.her@gmail.com>
2024-10-21 09:31:05 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,15 @@
+<?php
+/* CBTIS 89
+Programa que convierte los datos de un arrgelo en una cadena de texto
+Velazquez Hernandez Zoe Dylan
+3°A Programación Matutino
+*/
+echo "Datos Personales <br>";
+echo "<br>";
+echo " Nombre: Zoe Dylan velazquez Hernandez <br>";
+echo " Escuela : Centro de Barchillerato Tecnologico industial 89 <br>";
+echo " Carrera: Programación 3°A <br>";
+echo " Edad: 16 años <br>";
+echo " Ciudad, país :Durango,Mexico <br>";
+echo " Colonia: Lucio canañas <br>";
+?>
