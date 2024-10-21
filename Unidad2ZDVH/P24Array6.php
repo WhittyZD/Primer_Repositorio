cfe36513cad74158c491a5bf5c3295be4c6d968a
ZoeDylan <zoe.velazquez.her@gmail.com>
2024-10-21 10:34:02 -0600

    Update P24Array6.php

--- Unidad2ZDVH/P24Array6.php
+++ Unidad2ZDVH/P24Array6.php
@@ -1,10 +1,10 @@
 <?php
 /* CBTIS 89
-Programa para checar billetes
+Programa de arreglo donde guarda nombres, carrera ,escuela
 Velazquez Hernandez Zoe Dylan
 3°A Programación Matutino
 */
-$Personas = array("Programacion"=>'Lupe','arquitectura'=>'Mateo','Programcion'=>'Elva','Albañil'=>'Gina','Contabilidad'=>'Pepito');
+$Personas = array("Programacion"=>'Lupe','arquitectura'=>'Mateo','Programcion'=>'Elva','Albañil'=>'Toño','Contabilidad'=>'Pepito');
 
 echo "** CBTIS 89 **","<br>","<br>";
 foreach($Personas as $Carrera=>$losnombres) 
